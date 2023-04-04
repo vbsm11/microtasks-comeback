035b9aa6159bae7134e399b4d11cf848abb7c057
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-04-04 19:52:32 +0300

    Change src for new tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,163 +1,93 @@
-import React, {MouseEvent, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {Header} from './site/Header';
-import {Body} from './site/Body';
-import {Footer} from './site/Footer';
-import {NewComponent} from './components/NewComponent';
-import {Button} from './components/Button';
-import {FilterComponent} from './components/FilterComponent';
-import {FullInput} from './components/FullInput';
-import {Input} from './components/Input';
+import {Todolist} from './Todolist';
+import {v1} from 'uuid';
 
-export type StudentType = {
-    id: number,
-    name: string,
-    age: number
-}
-
-export type CarType = {
-    manufacturer: string,
-    model: string
-}
-
-export type MoneyType = {
-    banknots: string,
-    value: number,
-    number: string
-}
-
-export type FilterType = 'all' | 'RUBLS' | 'Dollars'
+export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
 
-    // const topCars: CarType[] = [
-    //     {manufacturer:'BMW', model:'m5cs'},
-    //     {manufacturer:'Mercedes', model:'e63s'},
-    //     {manufacturer:'Audi', model:'rs6'}
-    // ]
-    //
-    //
-    // const students: StudentType[] = [
-    //     {id: 1, name: 'James', age: 8},
-    //     {id: 2, name: 'Robert', age: 18},
-    //     {id: 3, name: 'John', age: 28},
-    //     {id: 4, name: 'Michael', age: 38},
-    //     {id: 5, name: 'William', age: 48},
-    //     {id: 6, name: 'David', age: 58},
-    //     {id: 7, name: 'Richard', age: 68},
-    //     {id: 8, name: 'Joseph', age: 78},
-    //     {id: 9, name: 'Thomas', age: 88},
-    //     {id: 10, name: 'Charles', age: 98},
-    //     {id: 11, name: 'Christopher', age: 108}
-    // ]
+    // let [tasks, setTasks] = useState([
+    //     {id: v1(), title: "HTML&CSS", isDone: true},
+    //     {id: v1(), title: "JS", isDone: true},
+    //     {id: v1(), title: "ReactJS", isDone: false},
+    //     {id: v1(), title: "Rest API", isDone: false},
+    //     {id: v1(), title: "GraphQL", isDone: false},
+    // ]);
+    // let [filter, setFilter] = useState<FilterValuesType>("all");
 
-    // const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-    //     console.log('Hi, I am 1st')
-    // }
-    //
-    // const mySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-    //     console.log('Hi, I am 2nd')
-    // }
+    let todolistID1=v1();
+    let todolistID2=v1();
 
-    // const onClickHandler = (name:string) => {
-    //     console.log(name)
-    // }
-
-    // const button1Foo = (sub: string, age: number, city: string) => {
-    //     console.log(sub, age, city)
-    // }
-    //
-    // const button2Foo = (sub: string) => {
-    //     console.log(sub)
-    // }
-    //
-    // const button3Foo = () => {
-    //     console.log('I am stupid button')
-    // }
-
-    // let [a, setA] = useState(1)
-    //
-    // const onClickHandler1 = () => {
-    //     setA(++a)
-    //     console.log(a)
-    // }
-    //
-    // const onClickHandler2 = () => {
-    //     setA(0)
-    // }
-
-
-
-    // const [money, setMoney] = useState<MoneyType[]>([
-    //     {banknots: 'Dollars', value: 100, number: ' a1234567890'},
-    //     {banknots: 'Dollars', value: 50, number: ' z1234567890'},
-    //     {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
-    //     {banknots: 'Dollars', value: 100, number: ' e1234567890'},
-    //     {banknots: 'Dollars', value: 50, number: ' c1234567890'},
-    //     {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
-    //     {banknots: 'Dollars', value: 50, number: ' x1234567890'},
-    //     {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
-    // ])
-    //
-    //
-    // const [filter, setFilter] = useState<FilterType>('all')
-    //
-    // let currentMoney: MoneyType[] = []
-    //
-    // if (filter === 'all') {
-    //     currentMoney = money
-    // }
-    // else if (filter === 'RUBLS' || 'Dollars') {
-    //     currentMoney = money.filter(m => m.banknots === filter)
-    // }
-    //
-    // const onclickFilterHandler = (filter: FilterType) => {
-    //     setFilter(filter)
-    // }
-
-    let [message, setMessage] = useState([
-        {message: 'message1'},
-        {message: 'message2'},
-        {message: 'message3'},
+    let [todolists, setTodolists] = useState<Array<todolistsType>>([
+        {id: todolistID1, title: 'What to learn', filter: 'all'},
+        {id: todolistID2, title: 'What to buy', filter: 'all'},
     ])
 
-    const [value, setValue] = useState('')
+    let [tasks, setTasks] = useState({
+        [todolistID1]:[
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "ReactJS", isDone: false},
+            {id: v1(), title: "Rest API", isDone: false},
+            {id: v1(), title: "GraphQL", isDone: false},
+        ],
+        [todolistID2]:[
+            {id: v1(), title: "HTML&CSS2", isDone: true},
+            {id: v1(), title: "JS2", isDone: true},
+            {id: v1(), title: "ReactJS2", isDone: false},
+            {id: v1(), title: "Rest API2", isDone: false},
+            {id: v1(), title: "GraphQL2", isDone: false},
+        ]
+    });
 
-    const addMessage = () => {
-        setMessage([{message: value}, ...message])
-        setValue('')
+
+
+    function removeTask(id: string) {
+        let filteredTasks = tasks.filter(t => t.id != id);
+        setTasks(filteredTasks);
     }
 
+    function addTask(title: string) {
+        let task = {id: v1(), title: title, isDone: false};
+        let newTasks = [task, ...tasks];
+        setTasks(newTasks);
+    }
+
+    function changeStatus(taskId: string, isDone: boolean) {
+        let task = tasks.find(t => t.id === taskId);
+        if (task) {
+            task.isDone = isDone;
+        }
+
+        setTasks([...tasks]);
+    }
+
+
+    let tasksForTodolist = tasks;
+
+    if (filter === "active") {
+        tasksForTodolist = tasks.filter(t => t.isDone === false);
+    }
+    if (filter === "completed") {
+        tasksForTodolist = tasks.filter(t => t.isDone === true);
+    }
+
+    function changeFilter(value: FilterValuesType) {
+        setFilter(value);
+    }
+
+
     return (
-        <div className={'App'}>
-            {/*<Header titleForHeader={'New header'}/>*/}
-            {/*<Body titleForBody={'New body'}/>*/}
-            {/*<Footer titleForFooter={'New footer'}/>*/}
-
-            {/*<NewComponent students={students} cars={topCars}/>*/}
-
-            {/*<button onClick={(event) => onClickHandler('1st')}>MyYouTubeChanel-1</button>*/}
-            {/*<button onClick={(event) => onClickHandler('2nd')}>MyYouTubeChanel-2</button>*/}
-
-            {/*<Button name={'MyYouTubeChanel-1'} callBack={() => button1Foo('1st', 21, 'Minsk')}/>*/}
-            {/*<Button name={'MyYouTubeChanel-2'} callBack={() => button2Foo('2nd')}/>*/}
-            {/*<Button name={'StupidButton'} callBack={button3Foo}/>*/}
-
-            {/*<h1>{a}</h1>*/}
-            {/*<button onClick={onClickHandler1}>number</button>*/}
-            {/*<button onClick={onClickHandler2}>0</button>*/}
-
-            {/*<FilterComponent currentMoney={currentMoney} onClickHandler={onclickFilterHandler}/>*/}
-
-            {/*<FullInput callBack={addMessage}/>*/}
-
-            <Input value={value} callBack={setValue}/>
-            <Button name={'+'} callBack={addMessage}/>
-            {message.map((m, index) => {
-                return (
-                    <div key={index}>{m.message}</div>
-                )
-            })}
+        <div className="App">
+            <Todolist title="What to learn"
+                      tasks={tasksForTodolist}
+                      removeTask={removeTask}
+                      changeFilter={changeFilter}
+                      addTask={addTask}
+                      changeTaskStatus={changeStatus}
+                      filter={filter}
+            />
         </div>
     );
 }
